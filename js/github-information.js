3ca319fd15ac8d84d4a84edd560735d38c2d6af5
Eugénie <eugenie.b24@hotmail.fr>
2023-10-14 17:42:55 +0100

    Update fetchGitHubInformation func to include promise to GitHub API

--- js/github-information.js
+++ js/github-information.js
@@ -10,4 +10,22 @@ function fetchGitHubInformation(event) {
         `<div id="loader">
             <img src="images/loader.gif" alt="loading..." />
         </div>`);
+
+     $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function(response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<p>No info found for user ${username}</p>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<p>Error: ${errorResponse.responseJSON.message}</p>`);
+            }
+        });
 }
