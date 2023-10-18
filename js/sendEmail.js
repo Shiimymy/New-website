e0383580be1f7893d9a26d8c661119d8f92a39d7
Eugénie <eugenie.b24@hotmail.fr>
2023-10-18 14:26:42 +0100

    Fix 412 error from EmailJS

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("service_7vhy7ru", "ebvn_website", {
+    emailjs.send("service_11ktij9", "request_website", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
@@ -7,9 +7,11 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            alert('Request Sent, Eugénie will come back to you as soon as possible!');
         },
         function(error) {
             console.log("FAILED", error);
+            alert('Please try again');
         }
     );
     return false;  // To block from loading a new page
